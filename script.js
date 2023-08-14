67bdb375cbe3736d269d81a34989244619710070
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-08-14 20:53:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const age = document.getElementById("age").value;
+   const name = document.getElementById("name").value;
+	let promise = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age > 18)
+			{
+				resolve(name);
+			}
+			else
+			{
+				reject(name);
+			}
+		}, 4000)
+	})
+
+	promise.then((data)=> {
+	alert(`Welcome, ${data}. You can vote.`);
+})
+
+promise.catch((data)=> {
+	alert(`Oh sorry ${data}. You aren't old enough.`);
+})
+	
+})
+
+
+
+
